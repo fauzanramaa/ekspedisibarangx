afebfa8fdfaf8cec5502c5aa3d823eb32d3dc33e
fauzanramaa <fauzaneramadane@gmail.com>
2024-12-01 13:37:39 +0700

    Setup Env

---
+++ Documents/NetBeansProjects/ekspedisibarangx/src/main/java/com/ekspedisi/Ekspedisibarangx.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ekspedisi;
+
+/**
+ *
+ * @author Fauzanramaa
+ */
+public class Ekspedisibarangx {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
